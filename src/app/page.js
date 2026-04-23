09c0ff85812f0bd49a7f00e9a41d777cb559e4cb
Harman Soni <harman.codes.dev@gmail.com>
2026-04-23 09:52:21 +0530

    Added Section[Who Am I, How I Work, Failures, Contact Me]

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
 import CustomScroll from "./components/CustomScrollbar";
+import WhoAmI from "./components/sections/AboutMe/AboutMe";
+import ContactMe from "./components/sections/Contact/ContactMe";
 import HeroSection from "./components/sections/HeroSection/HeroSection";
 import ProjectSection from "./components/sections/Projects/Projects";
+import HowIWork from "./components/sections/HowIWork/HowIwork";
+import Failures from "./components/sections/Failures/Failures";
 
 export default function Home() {
   return (
     <>
       <CustomScroll />
       <HeroSection />
+      <WhoAmI />
       <ProjectSection />
+      <HowIWork />
+      <Failures />
+      <ContactMe />
     </>
   );
 }
