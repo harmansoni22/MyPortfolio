cf5e00bc93646a18e546a3a6a8f4cac9f321eb95
Harman Soni <harman.codes.dev@gmail.com>
2026-04-25 18:03:26 +0530

    Next config file update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "n9ks10qx-3000.inc1.devtunnels.ms/"
+      ]
+    }
+  }
 };
 
 export default nextConfig;
