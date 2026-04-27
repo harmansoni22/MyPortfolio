96e60fb43d7e8187062eb9809fccdacc2cbca897
Harman Soni <harman.codes.dev@gmail.com>
2026-04-27 10:13:03 +0530

    Uploading robots.js for search crawlers

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: `${baseUrl}/sitemap.xml`,
+        host: baseUrl,
+    };
+}
